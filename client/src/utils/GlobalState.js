90a5bf1252ed91a5da88f2579ca22b171e02ff30
AliciaJK <alicia.krolak4@gmail.com>
2021-07-25 10:27:18 -0400

    changing global state file

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,14 +1,19 @@
-import React from "react";
-
-import { Provider } from 'react-redux';
-import store from './store';
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from './reducers'
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
 
-const StoreProvider = ({ ...props }) => {
+const StoreProvider = ({ value = [], ...props }) => {
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    cart: [],
+    cartOpen: false,
+    categories: [],
+    currentCategory: '',
+  });
 
-  return <Provider store={store} {...props} />;
+  return <Provider value={[state, dispatch]} {...props} />;
 };
 
 const useStoreContext = () => {
